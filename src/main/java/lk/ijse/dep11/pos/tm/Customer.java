ea0cab3a1f9dc6f4e842d78e6ffa8fd23600e558
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-10-16 21:01:44 +0530

    Add customer table model

---
+++ src/main/java/lk/ijse/dep11/pos/tm/Customer.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep11.pos.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private String id;
+    private String address;
+    private String name;
+}
