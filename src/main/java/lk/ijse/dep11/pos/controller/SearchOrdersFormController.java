d5a049f94f7b02f7a253f9c9ba04e9240d0bde7c
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-10-21 23:19:05 +0530

    Add order table model and map it with table

--- src/main/java/lk/ijse/dep11/pos/controller/SearchOrdersFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/SearchOrdersFormController.java
@@ -6,17 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep11.pos.tm.Order;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class SearchOrdersFormController {
     public TextField txtSearch;
-    public TableView tblOrders;
+    public TableView<Order> tblOrders;
     public AnchorPane root;
+
+    public void initialize(){
+        String[] cols = {"orderId","orderDate","customerId","customerName","total"};
+        for (int i = 0; i <cols.length ; i++) {
+            tblOrders.getColumns().get(i).setCellValueFactory(new PropertyValueFactory<>(cols[i]));
+
+        }
+    }
 
     public void tblOrders_OnMouseClicked(MouseEvent mouseEvent) {
     }
