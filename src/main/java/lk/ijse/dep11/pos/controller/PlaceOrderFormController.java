82b1e9606cbcd04b30db82596aa8ffe31bba1a1b
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-10-24 07:38:04 +0530

    Finalize pos bill design and finish the Smart Pos

--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
@@ -22,6 +22,11 @@ import lk.ijse.dep11.pos.db.OrderDataAccess;
 import lk.ijse.dep11.pos.tm.Customer;
 import lk.ijse.dep11.pos.tm.Item;
 import lk.ijse.dep11.pos.tm.OrderItem;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.awt.*;
 import java.io.IOException;
@@ -31,7 +36,9 @@ import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class PlaceOrderFormController {
@@ -198,11 +205,60 @@ public class PlaceOrderFormController {
                    lblId.getText().replace("Order ID:","").strip(),
                     Date.valueOf(lblDate.getText()),
                     cmbCustomerId.getValue().getId());
+            printBill();
             newOrder();
         } catch (SQLException e) {
             new Alert(Alert.AlertType.ERROR,"Failed to save the order,try again");
 
         }
+    }
+
+    private void printBill(){
+
+        try {
+            JasperDesign jasperDesign = JRXmlLoader
+                    .load(getClass().getResourceAsStream("/print/my-pos-bill.jrxml"));
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+
+            Map<String, Object> reportParams = new HashMap<>();
+            reportParams.put("id", lblId.getText().replace("Order ID: ", "").strip());
+            reportParams.put("date", lblDate.getText());
+            reportParams.put("customer-id", cmbCustomerId.getValue().getId());
+            reportParams.put("customer-name", cmbCustomerId.getValue().getName());
+            reportParams.put("total", lblTotal.getText());
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParams,
+                    new JRBeanCollectionDataSource(tblOrderDetails.getItems()));
+
+            JasperViewer.viewReport(jasperPrint, false);
+            // JasperPrintManager.printReport(jasperPrint, false);
+        } catch (JRException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to print the bill").show();
+        }
+
+
+/*        try {
+            JasperDesign jasperDesign = JRXmlLoader
+                    .load(getClass().getResourceAsStream("/print/pos-bill.jrxml"));
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+
+            Map<String, Object> reportParams = new HashMap<>();
+            reportParams.put("id", lblId.getText().replace("Order ID: ", "").strip());
+            reportParams.put("date", lblDate.getText());
+            reportParams.put("customer-id", cmbCustomerId.getValue().getId());
+            reportParams.put("customer-name", cmbCustomerId.getValue().getName());
+            reportParams.put("total", lblTotal.getText());
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParams,
+                    new JRBeanCollectionDataSource(tblOrderDetails.getItems()));
+
+            JasperViewer.viewReport(jasperPrint, false);
+            // JasperPrintManager.printReport(jasperPrint, false);
+        } catch (JRException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to print the bill").show();
+        }*/
     }
 
 }
